8616f385f86c6b3b7fc48a30c6d794a1160f4d4c
likun <1023673687@qq.com>
2022-08-03 21:35:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Hello.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 孤城揽月
+ * @description
+ * @create 2022-08-03 21:33
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,git");
+    }
+}
