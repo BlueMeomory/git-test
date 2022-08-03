0a6658a1394c05fa68a0c8c72cdd350b3dcf0a31
likun <1023673687@qq.com>
2022-08-03 22:53:57 +0800

    commit1

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -7,9 +7,6 @@ package com.atguigu.git;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello,git");
-        System.out.println("Hello,git2");
-        System.out.println("Hello,git3");
-        System.out.println("master test!");
+        System.out.println("go go go");
     }
 }
