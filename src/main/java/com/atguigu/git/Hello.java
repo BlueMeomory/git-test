bb7a5177157bd5229057c0e632dcb8046a3b8e98
BlueMeomory <109970535+BlueMeomory@users.noreply.github.com>
2022-08-04 00:22:04 +0800

    123

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("We are 伐木累");
         System.out.println("go go go");
+        System.out.println("123");
     }
 }
